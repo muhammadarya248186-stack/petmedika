9cdda3e6b6a33ad03745d8ba4bca6fbf6744d1b3
Maliqz <malikmas844@gmail.com>
2026-03-09 14:41:41 +0700

    Create kartu-antrian.php

---
+++ kartu-antrian.php
@@ -0,0 +1,39 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kartu Antrian</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!-- SIDEBAR -->
+    <aside class="sidebar">
+      <div class="logo">
+        <img src="image/logopet.png" alt="PetMedika Logo" />
+        <hr />
+      </div>
+
+      <nav>
+        <a href="#" >Ambil<br />Antrian</a>
+        <a href="#" >Daftar<br />Antrian</a>
+        <a href="#" class="active">Kartu<br />Antrian</a>
+      </nav>
+    </aside>
+
+    <!-- MAIN -->
+    <main class="main">
+      <h2>Kartu Antrian</h2>
+      <h1><span class="pet">Pet</span><span class="medika">Medika</span></h1>
+
+      <div class="bottom-image">
+        <img src="image/logokartu.png" alt="Ilustrasi PetMedika" />
+      </div> 
+    </main>
+</body>
+</html>
