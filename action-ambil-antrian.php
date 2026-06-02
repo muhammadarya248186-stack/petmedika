c3589e0d3420bf4f1cc673abfa3810b8feda3a9a
Maliqz <malikmas844@gmail.com>
2026-06-02 23:26:19 +0700

    Update action-ambil-antrian.php

--- action-ambil-antrian.php
+++ action-ambil-antrian.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include "./config.php";
 
 if(isset($_POST['service_id'])){
@@ -6,6 +7,10 @@ if(isset($_POST['service_id'])){
   $no_telp = $_POST['no_telp'];
   $service_id = $_POST['service_id'];
   $tanggal = date('Y-m-d');
+
+  // Simpan nomor telepon di session hanya untuk membantu filter kartu.
+  // Tidak mengubah struktur database.
+  $_SESSION['last_no_telp'] = $no_telp;
 
   // ambil nomor terakhir per loket & tanggal
   $cek = mysqli_query($conn, "
@@ -28,6 +33,8 @@ if(isset($_POST['service_id'])){
     ('$service_id', '$no_telp', '$nomor', '$tanggal', 'waiting', NOW())
   ");
 
+  // Setelah ambil antrian, pindah ke halaman kartu.
+  // kartu-antrian.php akan menampilkan semua antrian hari ini.
   header("Location: kartu-antrian.php");
   exit;
 }
